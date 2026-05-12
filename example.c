37a3b81db790a9021824a74c8bf2477720c9c6cc
Deva Midhun <admin@coder.is-a.dev>
2026-05-12 11:20:10 +0530

    did some patches and now no need to manually call termmodes

--- example.c
+++ example.c
@@ -6,22 +6,6 @@
 
 struct termios orig_termios;
 
-void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
-}
-
-void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
-    atexit(disableRawMode); // Ensure restore on crash/exit
-
-    struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
-    /* Disable flow control (Ctrl-S/Ctrl-Q) and CR to NL mapping */
-    raw.c_iflag &= ~(IXON | ICRNL);
-    
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-}
-
 void my_completion(const char *buf, zl_completions_t *lc) {
     // List of all possible commands
     const char *commands[] = {"ls", "lscpu", "lsblk", "znu-kernel", NULL};
@@ -38,8 +22,6 @@ void my_completion(const char *buf, zl_completions_t *lc) {
 int main() {
     printf("ZLINE 2.0 - Press Ctrl-C to exit, Ctrl-R to search\n");
 
-    enableRawMode();
-
     zline_t *zl = zline_init("\033[1;32mzline\033[0m> ");
     zline_set_completion_callback(zl, my_completion);
 
@@ -51,9 +33,7 @@ int main() {
             break;
         }
 
-        disableRawMode();
         printf("You entered: [%s]\n", line);
-        enableRawMode();
     }
 
     return 0;
